ec1bf2bf3810dba2e0645c72ed490888dd6e95e4
Behnaz Bagheri <behnaz.tadi@gmail.com>
2019-12-24 12:04:41 +0100

    Comment to demonstrate why I create ExchangeRates as Web API

--- CryptoConvertor/CryptoConvertor.Services.ExchnageRates/Controllers/ValuesController.cs
+++ CryptoConvertor/CryptoConvertor.Services.ExchnageRates/Controllers/ValuesController.cs
@@ -11,36 +11,15 @@ namespace CryptoConvertor.Services.ExchnageRates.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        //
+        // It can be an endpoint to load exchange rates, It could be console as well
+        //
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {            
             return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        }        
     }
 }
